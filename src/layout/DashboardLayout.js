3ffaaebc1a5b10d77255778e8278ef599695be76
Marxie <jhomsabdulmukim@gmail.com>
2025-04-03 09:27:43 +0800

    Updated project with appbar

--- src/layout/DashboardLayout.js
+++ src/layout/DashboardLayout.js
@@ -3,24 +3,26 @@ import { Routes, Route, Link } from 'react-router-dom';
 import TrainingManagement from '../pages/admin/TrainingManagement';
 import Dashboard from '../pages/admin/Dashboard';
 import Registration from '../pages/admin/Registration';
+import DrawerAppBar from '../components/AppBar';
 
 const DashboardLayout = () => {
   return (
-    <Container maxWidth="lg">
-      <Box sx={{ py: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          GAD Information System
-        </Typography>
-        <Box sx={{ mb: 2 }}>
-          <Link to="/">Home</Link> | <Link to="/training">Training</Link> | <Link to="/registration">Register</Link> | <Link to="/landing">Logout</Link>
-        </Box>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/training" element={<TrainingManagement/>}/>
-          <Route path="/registration" element={<Registration/>} />
-        </Routes>
-      </Box>
-    </Container>
+    // <Container maxWidth="lg">
+    //   <Box sx={{ py: 4 }}>
+    //     <Typography variant="h4" gutterBottom>
+    //       GAD Information System
+    //     </Typography>
+    //     <Box sx={{ mb: 2 }}>
+    //       <Link to="/">Home</Link> | <Link to="/training">Training</Link> | <Link to="/registration">Register</Link> | <Link to="/landing">Logout</Link>
+    //     </Box>
+    //     <Routes>
+    //       <Route path="/" element={<Dashboard />} />
+    //       <Route path="/training" element={<TrainingManagement/>}/>
+    //       <Route path="/registration" element={<Registration/>} />
+    //     </Routes>
+    //   </Box>
+    // </Container>
+    <DrawerAppBar/>
   );
 };
 
